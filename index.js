07d9073fc660770e7c3448f54f71353cc6bbf50c
Turki <teko_911@hotmail.com>
2024-01-22 16:40:24 +0300

    fix: correct yaml file

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
-// Environment variable POD_NAME will be set by Kubernetes
+const PORT = process.env.PORT || 3000;
 const podName = process.env.POD_NAME || 'Unknown';
-
-// Serve the HTML file
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html'); // Assuming your HTML file is named index.html
-});
-
-// Handle the /greet endpoint
-app.get('/greet1', (req, res) => {
+// Serve the greeting message
+app.get('/greet', (req, res) => {
   res.send(`Hello from Node.js App! Served by Pod: ${podName}`);
 });
 
-app.listen(port, () => {
-  console.log(`Node.js app listening at http://localhost:${port}`);
+// Serve static files (for HTML, CSS, JS)
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
