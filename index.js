230cc5e3dad26300c159905600e507b9c99bf850
Turki <teko_911@hotmail.com>
2024-01-22 14:32:22 +0300

    init 3

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+// Serve the greeting message
+app.get('/greet', (req, res) => {
   res.send(`Hello from ${PORT}`);
 });
+
+// Serve static files (for HTML, CSS, JS)
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
