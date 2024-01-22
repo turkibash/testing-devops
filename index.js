5229148fb741637762f13898c70bde3f3d65f116
Turki <teko_911@hotmail.com>
2024-01-22 16:42:27 +0300

    fix: correct yaml file

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const podName = process.env.POD_NAME || 'Unknown';
-// Serve the greeting message
-app.get('/greet', (req, res) => {
-  res.send(`Hello from Node.js App! Served by Pod: ${podName}`);
-});
 
-// Serve static files (for HTML, CSS, JS)
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.send(`Hello from ${PORT}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
