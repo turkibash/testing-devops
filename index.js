d6133aee7736f4b2997e2557594bf977a9c5bce3
Turki <teko_911@hotmail.com>
2024-01-22 17:03:42 +0300

    fix: correct yaml file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send(`Hello from ${PORT}`);
